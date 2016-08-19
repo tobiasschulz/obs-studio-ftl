03fa1fab7e2cf85521cad8dcc6fb18fe0a5a82c8
sslivins <sslivins@gmail.com>
2016-08-18 17:46:03 -0700

    generate 32 bit, 64bit and installer

--- build_script/build.bat
+++ build_script/build.bat
@@ -1,18 +1,26 @@
 SET build_config=Release
-SET obs_version=1.2.11
+SET obs_version=0.15.4-ftl
 SET coredeps=C:\beam\tachyon_deps
 SET QTDIR64=C:\Qt\5.6\msvc2015_64
+SET QTDIR32=C:\Qt\5.6\msvc2015
+SET obsInstallerTempDir=%cd:\=/%/rundir/%build_config%
 SET PATH=%PATH%;C:\Program Files (x86)\MSBuild\14.0\Bin;C:\Program Files (x86)\CMake\bin
+SET DepsPath32=%coredeps%\win32
 SET DepsPath64=%coredeps%\win64
-SET FFmpegPath64=%coredeps%\win64
-SET x264Path64=%coredeps%\win64
-SET curlPath64=%coredeps%\win64
 pushd .
 cd ..
 call git submodule update --init
 popd
-cmake -G "Visual Studio 14 2015 Win64" -DOBS_VERSION_OVERRIDE=%obs_version% -DFTLSDK_LIB=%ftl_lib_dir% -DFTLSDK_INCLUDE_DIR=%ftl_inc_dir% -DCOPIED_DEPENDENCIES=false -DCOPY_DEPENDENCIES=true ..
+cmake -G "Visual Studio 14 2015" -DOBS_VERSION_OVERRIDE=%obs_version% -DCOPIED_DEPENDENCIES=false -DCOPY_DEPENDENCIES=true ..
+call msbuild /p:Configuration=%build_config% ALL_BUILD.vcxproj || exit /b
+copy %coredeps%\win32\bin\postproc-54.dll rundir\%build_config%\bin\32bit
+rmdir CMakeFiles /s /q
+del CMakeCache.txt
+cmake -G "Visual Studio 14 2015 Win64" -DOBS_VERSION_OVERRIDE=%obs_version% -DCOPIED_DEPENDENCIES=false -DCOPY_DEPENDENCIES=true ..
 call msbuild /p:Configuration=%build_config%,Platform=x64 ALL_BUILD.vcxproj || exit /b
-copy %coredeps%\win64\bin\postproc-54.dll rundir\Debug\bin\64bit
-copy %coredeps%\win64\bin\postproc-54.dll rundir\Release\bin\64bit
+copy %coredeps%\win64\bin\postproc-54.dll rundir\%build_config%\bin\64bit
+del CMakeCache.txt
+rmdir CMakeFiles /s /q
+cmake -G "Visual Studio 14 2015" -DOBS_VERSION_OVERRIDE=%obs_version% -DCOPIED_DEPENDENCIES=false -DCOPY_DEPENDENCIES=true -DINSTALLER_RUN=true ..
+call msbuild /p:Configuration=%build_config% PACKAGE.vcxproj || exit /b
 
