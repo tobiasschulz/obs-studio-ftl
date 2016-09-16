c269db3f3bc2d046026f62251dd464e541f98119
Stefan Slivinski <sslivins@gmail.com>
2016-09-15 21:29:18 -0700

    fixed build script

--- build_script/build.bat
+++ build_script/build.bat
@@ -1,31 +1,27 @@
 REM @Echo Off
 SET build_config=Release
-SET obs_version=0.15.4-ftl.2
+SET obs_version=0.15.4-ftl.3
 SET coredeps=C:\beam\tachyon_deps
 SET QTDIR64=C:\Qt\5.6\msvc2015_64
 SET QTDIR32=C:\Qt\5.6\msvc2015
-SET obsInstallerTempDir=%cd:\=/%/rundir/%build_config%
 SET PATH=%PATH%;C:\Program Files (x86)\MSBuild\14.0\Bin;C:\Program Files (x86)\CMake\bin
+SET startingPath=%cd%
 SET DepsPath32=%coredeps%\win32
 SET DepsPath64=%coredeps%\win64
-SET starting_dir=%cd%
-
 SET build32=
 SET build64=
 SET package=
+
 if "%1" == "all" (
-	SET build32=true
-	SET build64=true
-	SET package=true
-)
-if "%1" == "win32" (
-	SET build32=true
+SET build32=true
+SET build64=true
+SET package=true
 )
 if "%1" == "win64" (
-	SET build64=true
+SET build64=true
 )
 if "%1" == "package" (
-	SET package=true
+SET package=true
 )
 if "%1" == "clean" (
    IF NOT EXIST ..\build GOTO NOBUILDDIR
@@ -37,6 +33,7 @@ pushd .
 cd ..
 call git submodule update --init
 pushd .
+cd ..
 call :SUB_FTLSDK
 popd .
 IF EXIST build GOTO BUILD_DIR_EXISTS
@@ -44,27 +41,33 @@ mkdir build
 :BUILD_DIR_EXISTS
 cd build
 if defined build32 (
-	echo %cd%
-	rmdir CMakeFiles /s /q
+	ECHO Building 32bit OBS-Studio FTL
+	echo Currently in Directory %cd%	
+    rmdir CMakeFiles /s /q
 	del CMakeCache.txt
-	call cmake -G "Visual Studio 14 2015" -DOBS_VERSION_OVERRIDE=%obs_version% -DCOPIED_DEPENDENCIES=false -DCOPY_DEPENDENCIES=true -DFTLSDK_INCLUDE_DIR=%ftl_inc_dir% -DFTLSDK_LIB=%ftl_lib_dir%  .. || goto DONE
+	cmake -G "Visual Studio 14 2015" -DOBS_VERSION_OVERRIDE=%obs_version% -DCOPIED_DEPENDENCIES=false -DCOPY_DEPENDENCIES=true -DFTLSDK_INCLUDE_DIR=%ftl_inc_dir% -DFTLSDK_LIB=%ftl_lib_dir% .. || goto DONE
 	call msbuild /p:Configuration=%build_config% ALL_BUILD.vcxproj
 	copy %coredeps%\win32\bin\postproc-54.dll rundir\%build_config%\bin\32bit
 )
 if defined build64 (
-	rmdir CMakeFiles /s /q
+	ECHO Building 64bit OBS-Studio FTL
+	echo Currently in Directory %cd%	
+    rmdir CMakeFiles /s /q
 	del CMakeCache.txt
-	call cmake -G "Visual Studio 14 2015 Win64" -DOBS_VERSION_OVERRIDE=%obs_version% -DCOPIED_DEPENDENCIES=false -DCOPY_DEPENDENCIES=true -DFTLSDK_INCLUDE_DIR=%ftl_inc_dir% -DFTLSDK_LIB=%ftl_lib_dir% .. || goto DONE
+	cmake -G "Visual Studio 14 2015 Win64" -DOBS_VERSION_OVERRIDE=%obs_version% -DCOPIED_DEPENDENCIES=false -DCOPY_DEPENDENCIES=true -DFTLSDK_INCLUDE_DIR=%ftl_inc_dir% -DFTLSDK_LIB=%ftl_lib_dir% .. || goto DONE
 	call msbuild /p:Configuration=%build_config%,Platform=x64 ALL_BUILD.vcxproj || goto DONE
 	copy %coredeps%\win64\bin\postproc-54.dll rundir\%build_config%\bin\64bit
 )
 if defined package (
-	rmdir CMakeFiles /s /q
+	ECHO Packaging OBS-Studio FTL
+	echo Currently in Directory %cd%	
+	SET obsInstallerTempDir=%cd:\=/%/rundir/%build_config%
+    rmdir CMakeFiles /s /q
 	del CMakeCache.txt
-	call cmake -G "Visual Studio 14 2015" -DOBS_VERSION_OVERRIDE=%obs_version% -DCOPIED_DEPENDENCIES=false -DCOPY_DEPENDENCIES=true -DINSTALLER_RUN=true ..
+	cmake -G "Visual Studio 14 2015" -DOBS_VERSION_OVERRIDE=%obs_version% -DINSTALLER_RUN=true ..
 	call msbuild /p:Configuration=%build_config% PACKAGE.vcxproj || goto DONE
 )
-GOTO:EOF 
+GOTO DONE
 :SUB_FTLSDK
     ECHO Building FTL SDK
 	pushd .
@@ -73,19 +76,12 @@ GOTO:EOF
 	call git checkout xsplit
 	mkdir build
 	cd build
-	if defined build32 (	
-		call cmake -G "Visual Studio 14 2015" .. || goto DONE
-		call msbuild /p:Configuration=%build_config%,ALL_BUILD.vcxproj || goto DONE
-		SET ftl_lib_dir=%cd%\%build_config%\ftl.lib
-		SET ftl_inc_dir=%cd%\..\libftl
-	)
-	if defined build64 (	
-		call cmake -G "Visual Studio 14 2015 Win64" .. || goto DONE
-		call msbuild /p:Configuration=%build_config%,Platform=x64 ALL_BUILD.vcxproj || goto DONE
-		SET ftl_lib_dir=%cd%\%build_config%\ftl.lib
-		SET ftl_inc_dir=%cd%\..\libftl
-	)	
+	call cmake -G "Visual Studio 14 2015 Win64" .. || goto DONE
+	call msbuild /p:Configuration=%build_config%,Platform=x64 ALL_BUILD.vcxproj || goto DONE
+	SET ftl_lib_dir=%cd%\%build_config%\ftl.lib
+	SET ftl_inc_dir=%cd%\..\libftl	
 	popd .
 	GOTO:EOF 
 :DONE
-cd %starting_dir%
+cd %startingPath%
+
